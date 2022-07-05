d15b5ff7ea6afe9574609dc6895e0d1eaa83bf2c
ck-lamib <113ck.lamib@gmail.com>
2022-07-05 16:15:02 +0545

    adding variablescope.js file

---
+++ variableScope.js
@@ -0,0 +1,60 @@
+(function(){
+    var x = 20; // global variable
+    function sum(){
+        var y = 10;  // local variable
+        console.log("value of x: " + x++); // 20
+        console.log("value of y: " + y);  //10
+        return (x+y);
+    }
+    sum();
+    console.log("value of x: " + x); //21
+    console.log("value of y: " + y);  //undefine
+})();
+
+
+(function(){
+    var x = 20; // global variable
+    function sum(){
+        var x = 10;  // local variable
+        console.log("value of x: " + x++); // 10
+    }
+    sum();
+    console.log("value of x: " + x); //20
+})();
+
+(function(){
+    var x = 20; // global variable
+    function sum(){
+        x = 10; //changing value of x
+        console.log("value of x: " + x++); // 10
+    }
+    sum();
+    console.log("value of x: " + x); //11
+})();
+
+
+//var x is used multiple times
+(function(){
+    var x = 20; // global variable
+    var x = 40;
+    function sum(){
+        console.log("value of x: " + x++); // 40
+    }
+    sum();
+    console.log("value of x: " + x); //41
+})();
+
+//var x and let x cannot be used
+(function(){
+    var x = 20; // global variable
+    var x = 40;
+    let x = 30;
+    function sum(){
+        console.log("value of x: " + x++); // error
+    }
+    sum();
+    console.log("value of x: " + x); //error
+})();
+
+
+
