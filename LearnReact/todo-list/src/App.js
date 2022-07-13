addac65ddbf2a5c53bbbe9fcbf309ebccb134a39
ck-lamib <113ck.lamib@gmail.com>
2022-07-13 12:03:54 +0545

    committing after adding deleting and adding functionality task to todolist

--- LearnReact/todo-list/src/App.js
+++ LearnReact/todo-list/src/App.js
@@ -3,18 +3,37 @@ import React, {useState} from "react";
 import Todo from "./components/Todo";
 import FilterButton from "./components/FilterButton";
 import Form from "./components/Form";
+import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js'
 
 
 
 function App(props) {
   //callback props
 function addTask(name) {
-  const newTask = {name: name, complete:false, id:"id"};
+  const newTask = {name: name, complete:false, id:"todo-"+nanoid()};
   setTasks([...tasks, newTask]);
 }
  const [tasks, setTasks] = useState(props.tasks);
-  const taskList = tasks.map(task => <Todo name={task.name} complete={task.complete} id={task.id} />);
+
+ function toggleTaskCompleted(id) {
+  const updatedTask = tasks.map( task =>
+    {
+      if(id === task.id){
+        return {...task, complete: !task.complete}
+      }
+      return task;
+    });
+    setTasks(updatedTask);
+}
+function deleteTasks(id){
+  const remainingTasks = tasks.filter(task => id !== task.id);
+  setTasks(remainingTasks);
+}
+
+  const taskList = tasks.map(task => <Todo name={task.name} complete={task.complete} id={task.id} toggleTaskCompleted={toggleTaskCompleted} deleteTasks={deleteTasks}/>);
   const taskType = props.types?.map(type => <FilterButton taskType={type}/>);
+  const tasksNoun = (taskList.length === 1)? 'task':'tasks';
+  const headingText=`${taskList.length} ${tasksNoun} reamaning`;
   return (
     <div className="App">
       <div className="todoapp stack-large">
@@ -25,7 +44,7 @@ function addTask(name) {
           {taskType}
         </div>
 
-        <h2 id="list-heading">3 task reamaning</h2>
+        <h2 id="list-heading">{headingText}</h2>
         <ul role="list" aria-labelledby="list-heading" className="todo-list stack-large stack-exception">
           {taskList}
         </ul>
