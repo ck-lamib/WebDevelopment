04a7ffdfa65c2726e1529666dbdbad16b0639856
ck-lamib <113ck.lamib@gmail.com>
2022-07-19 00:25:17 +0545

    adding gridtutorial and committing modified app and index.css fiel

--- LearnReact/learn_hooks/src/App.js
+++ LearnReact/learn_hooks/src/App.js
@@ -1,10 +1,21 @@
-import { StateTutorial } from "./components/StateTutorial";
-function App() {
-  return (
-    <div className="App">
-      <StateTutorial/>
+import React from "react";
+import { GridTutorial } from "./components/GridTutorial";
+
+const App = () => {
+  const Datas = [1,2,3,4,5,6];
+  const classN = [];
+  const bi = "div" + Datas.map(data=> data);
+ 
+
+  const listData = Datas.map(data => <GridTutorial id={data}/>); 
+  return(
+    // <StateTutorial/>
+    <div>
+    {listData}
+    <button onClick={() =>{console.log(bi)}}>button</button>
     </div>
   );
-}
+};
 
 export default App;
+
---
+++ LearnReact/learn_hooks/src/components/GridTutorial.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const GridTutorial = (props) => {
+  return(
+    <div className={props.className}>
+        <p>div{props.id}</p>
+    </div>
+    );
+};
